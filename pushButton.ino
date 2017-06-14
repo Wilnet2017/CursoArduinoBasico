8fd7e6eec4b5c24aa19d7ef3c81411de347e7e99
Wilson Martínez Martínez <wilsonmartinezmartinez@gmail.com>
2017-06-14 09:51:02 -0500

    Add files via upload

---
+++ pushButton.ino
@@ -0,0 +1,24 @@
+int entrada = 7;
+int valor = 0;
+int LED = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(entrada,INPUT);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  valor = digitalRead(entrada);
+  
+  if(valor == HIGH)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+     digitalWrite(LED,LOW);
+  }
+
+}
